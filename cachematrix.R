e5aeaed6c6bfadeb2b5368f8d866d005e4f98cee
RaphSchw <raphies94@gmail.com>
2021-11-22 15:02:35 +0100

    Added comments to the functions in order to describe how they work

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The 2 functions (makeCacheMatrix and cacheSolve) enable the calculation and
+## storage of a matrix and its inverse in the cache and avoid recalculation if
+## they are present in the cache
 
 ## Write a short comment describing this function
+## The function :
+## 1. Creates the x_inverse variable
+## 2. Stores it and the matrix x in its environment
+## 3. Defines functions for:
+##    a) getting the value of the matrix from its environment
+##    b) calculating the inverse of the matrix and assigning it to the x_inverse 
+##       variable
+##    c) getting the value of the inversed matrix 
+##    d) creating the output list containing all previous elements
 
 makeCacheMatrix <- function(x = matrix()) {
   x_inverse <- NULL
@@ -16,7 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The function :
+## 1. Evaluates whether the x_inverse of the makeCacheMatrix function is in the 
+##    its environment
+## 2a: if the x_inverse value is in the cache this value is returned
+## 2b: if x_inverse is not in the function environment of makeCacheMatrix
+##     it is calculated and using the setinverse function from makeCacheMatrix,
+##     which stores it in its function environment 
 
 cacheSolve <- function(x, ...) {
   x_inverse <- x$getinverse()
